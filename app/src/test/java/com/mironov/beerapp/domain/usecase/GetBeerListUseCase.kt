24c759fc0bb998df8806374f924cda1ef520a419
Denis Mironov <denis5918@gmail.com>
2023-12-20 20:59:40 +0700

    рефактор

--- app/src/test/java/com/mironov/beerapp/domain/usecase/GetBeerListUseCase.kt
+++ app/src/test/java/com/mironov/beerapp/domain/usecase/GetBeerListUseCase.kt
@@ -1,13 +1,13 @@
 package com.mironov.beerapp.domain.usecase
 
-import com.mironov.beerapp.util.BeerData
 import com.mironov.beerapp.domain.repository.BeerRepository
-import com.mironov.beerapp.domain.usecase.GetBeerListUseCase
+import com.mironov.beerapp.util.BeerData
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
-import org.junit.jupiter.api.Assertions.assertEquals
 
 class GetBeerListUseCaseTest {
 
@@ -17,7 +17,7 @@ class GetBeerListUseCaseTest {
     private val beerList = BeerData.beerList
 
     @Test
-    fun `invoke EXPECT list beer` = runTest {
+    fun `invoke EXPECT list beer`() = runTest {
         whenever(repository.getList()) doReturn beerList
 
         val expected = beerList
