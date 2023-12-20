80d78d3f92c662da819cd7d52f2ff915556c0d6c
Denis Mironov <denis5918@gmail.com>
2023-12-20 21:02:55 +0700

    Добавил use case на получение пива по id

--- app/src/main/java/com/mironov/beerapp/domain/usecase/GetBeerByIdUseCase.kt
+++ app/src/main/java/com/mironov/beerapp/domain/usecase/GetBeerByIdUseCase.kt
@@ -2,7 +2,7 @@ package com.mironov.beerapp.domain.usecase
 
 import com.mironov.beerapp.domain.repository.BeerRepository
 
-class GetBeerById (private val repository: BeerRepository) {
+class GetBeerByIdUseCase (private val repository: BeerRepository) {
 
     suspend operator fun invoke(id: Long) = repository.getById(id)
 }
