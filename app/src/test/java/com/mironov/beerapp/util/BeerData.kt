cba23dac4aedeb6787c4300852f71e63c93e7e65
Denis Mironov <denis5918@gmail.com>
2023-12-21 18:10:36 +0700

    test map dto to db model

--- app/src/test/java/com/mironov/beerapp/util/BeerData.kt
+++ app/src/test/java/com/mironov/beerapp/util/BeerData.kt
@@ -8,7 +8,7 @@ object BeerData {
 
     private val foodPairing = listOf("f1", "f2", "f3")
 
-    private val beer = Beer(
+    val beer = Beer(
         id = 1L,
         name = "name",
         tagline = "tagline",
@@ -21,6 +21,34 @@ object BeerData {
         ph = 1f,
         foodPairing = foodPairing,
     )
+
+     val beerDto = BeerDto(
+        id = 1L,
+        name = "name",
+        tagline = "tagline",
+        description = "description",
+        imageUrl = "imageUrl",
+        abv = 1f,
+        ibu = 1f,
+        ebc = 1f,
+        srm = 1f,
+        ph = 1f,
+        foodPairing = foodPairing,
+    )
+    val beerDbModel = BeerDbModel(
+        id = 1L,
+        name = "name",
+        tagline = "tagline",
+        description = "description",
+        imageUrl = "imageUrl",
+        abv = 1f,
+        ibu = 1f,
+        ebc = 1f,
+        srm = 1f,
+        ph = 1f,
+        foodPairing = foodPairing,
+    )
+
 
     private val beerList = listOf(beer, beer, beer)
 
