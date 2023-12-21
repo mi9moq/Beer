11c40330dc4f0b16a0fb4981a6353b473ef95d11
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-21 18:41:13 +0700

    Добавил маппер

---
+++ app/src/main/java/com/mironov/beerapp/data/mapper/BeerMapper.kt
@@ -0,0 +1,36 @@
+package com.mironov.beerapp.data.mapper
+
+import com.mironov.beerapp.data.database.model.BeerDbModel
+import com.mironov.beerapp.data.network.model.BeerDto
+import com.mironov.beerapp.domain.entity.Beer
+
+class BeerMapper {
+
+    fun mapDtoToDb(from: BeerDto): BeerDbModel = BeerDbModel(
+        id = from.id,
+        name = from.name,
+        tagline = from.tagline,
+        description = from.description,
+        imageUrl = from.imageUrl,
+        abv = from.abv,
+        ibu = from.ibu,
+        ebc = from.ebc,
+        srm = from.srm,
+        ph = from.ph,
+        foodPairing = from.foodPairing,
+    )
+
+    fun mapDbToEntity(from: BeerDbModel): Beer = Beer(
+        id = from.id,
+        name = from.name,
+        tagline = from.tagline,
+        description = from.description,
+        imageUrl = from.imageUrl,
+        abv = from.abv,
+        ibu = from.ibu,
+        ebc = from.ebc,
+        srm = from.srm,
+        ph = from.ph,
+        foodPairing = from.foodPairing,
+    )
+}
