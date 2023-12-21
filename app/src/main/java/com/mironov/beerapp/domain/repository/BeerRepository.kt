bbee2cb2b27cca98c348d494d0ebcf32e56ef42f
Denis Mironov <denis5918@gmail.com>
2023-12-21 17:02:55 +0700

    Переписал репозиторий

--- app/src/main/java/com/mironov/beerapp/domain/repository/BeerRepository.kt
+++ app/src/main/java/com/mironov/beerapp/domain/repository/BeerRepository.kt
@@ -1,12 +1,13 @@
 package com.mironov.beerapp.domain.repository
 
 import com.mironov.beerapp.domain.entity.Beer
+import com.mironov.beerapp.domain.entity.Result
 
 interface BeerRepository {
 
-    suspend fun getList() : List<Beer>
+    suspend fun getList() : Result<List<Beer>>
 
-    suspend fun getById(id: Long): Beer
+    suspend fun getById(id: Long): Result<Beer>
 
-    suspend fun getRandom(): Beer
+    suspend fun getRandom(): Result<Beer>
 }
