61b1a237356ef124cb867273c336dc0634f586a7
Denis Mironov <denis5918@gmail.com>
2023-12-21 19:40:36 +0700

    LocalDataSource getList test

---
+++ app/src/test/java/com/mironov/beerapp/data/datasource/BeerLocalDataSourceImplTest.kt
@@ -0,0 +1,28 @@
+package com.mironov.beerapp.data.datasource
+
+import com.mironov.beerapp.data.datasource
+import com.mironov.beerapp.data.database.BeerDao
+import com.mironov.beerapp.util.BeerData
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import org.mockito.kotlin.doReturn
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class BeerLocalDataSourceImplTest {
+    private val dao: BeerDao = mock()
+    private val dataSource = BeerLocalDataSourceImpl(dao)
+
+    private val dbList = BeerData.dbList
+
+    @Test
+    fun `getList EXPECT get beer from dao`() = runTest {
+        whenever(dao.getList()) doReturn dbList
+
+        val expect = dbList
+        val actual = dataSource.getList()
+
+        assertEquals(expect, actual)
+    }
+}
