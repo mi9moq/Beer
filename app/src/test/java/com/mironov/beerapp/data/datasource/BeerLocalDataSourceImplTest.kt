1e14090e812473856530bcbbf1330cafb259ed5c
Denis Mironov <denis5918@gmail.com>
2023-12-21 19:42:39 +0700

    LocalDataSource set test

--- app/src/test/java/com/mironov/beerapp/data/datasource/BeerLocalDataSourceImplTest.kt
+++ app/src/test/java/com/mironov/beerapp/data/datasource/BeerLocalDataSourceImplTest.kt
@@ -9,6 +9,7 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import org.mockito.kotlin.doReturn
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.mockito.kotlin.verify
 
 class BeerLocalDataSourceImplTest {
     private val dao: BeerDao = mock()
@@ -24,5 +25,13 @@ class BeerLocalDataSourceImplTest {
         val actual = dataSource.getList()
 
         assertEquals(expect, actual)
+    }
+
+    @Test
+    fun `set EXPECT set beers to dao`() = runTest {
+
+        dataSource.set(dbList)
+
+        verify(dao).set(dbList)
     }
 }
