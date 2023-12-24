4c843be3a46a3dc156d817d7e70516a1063afaa6
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-24 19:24:46 +0700

    Дописал тесты на репозиторий

--- app/src/test/java/com/mironov/beerapp/data/repository/BeerRepositoryImplTest.kt
+++ app/src/test/java/com/mironov/beerapp/data/repository/BeerRepositoryImplTest.kt
@@ -1,27 +1,39 @@
 package com.mironov.beerapp.data.repository
 
-import com.mironov.beerapp.data.database.BeerDao
 import com.mironov.beerapp.data.datasource.BeerLocalDataSource
 import com.mironov.beerapp.data.datasource.BeerRemoteDataSource
 import com.mironov.beerapp.data.mapper.BeerMapper
-import com.mironov.beerapp.data.network.api.BeerApi
 import com.mironov.beerapp.domain.entity.Result
 import com.mironov.beerapp.util.BeerData
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import java.util.stream.Stream
 
 class BeerRepositoryImplTest {
 
+    private companion object {
+        @JvmStatic
+        fun connectionError(): Stream<Exception> = Stream.of(
+            UnknownHostException(),
+            ConnectException(),
+            SocketTimeoutException()
+        )
+    }
+
     private val remoteDataSource: BeerRemoteDataSource = mock()
     private val localDataSource: BeerLocalDataSource = mock()
-    private val dao: BeerDao = mock()
-    private val api: BeerApi = mock()
     private val mapper: BeerMapper = mock()
     private val dispatcher = StandardTestDispatcher()
     private val repository: BeerRepositoryImpl = BeerRepositoryImpl(
@@ -38,15 +50,8 @@ class BeerRepositoryImplTest {
     private val beerListDto = BeerData.dtoList
     private val beerListDbModel = BeerData.dbList
 
-//    @Before
-//    fun setUp() {
-//        repository = BeerRepositoryImpl(
-//            dispatcher = dispatcher,
-//            localDataSource = localDataSource,
-//            remoteDataSource = remoteDataSource,
-//            mapper = mapper,
-//        )
-//    }
+    private val unknownError = BeerData.unknownError
+    private val connectionError = BeerData.connectionError
 
     @Test
     @Suppress("ktlint:standard:max-line-length")
@@ -58,6 +63,113 @@ class BeerRepositoryImplTest {
 
         val expected = Result.Success(beerList)
         val actual = repository.getList()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    @Suppress("ktlint:standard:max-line-length")
+    fun `get list EXPECT insert in DB`() = runTest(dispatcher) {
+        whenever(remoteDataSource.getList()) doReturn beerListDto
+        whenever(mapper.mapDtoToDb(beerDto)) doReturn beerDbModel
+
+        repository.getList()
+
+        verify(localDataSource).set(beerListDbModel)
+    }
+
+    @ParameterizedTest
+    @Suppress("ktlint:standard:max-line-length")
+    @MethodSource("connectionError")
+    fun `get with Internet exception EXPECT connection error type`(e: Exception) =
+        runTest(dispatcher) {
+            whenever(remoteDataSource.getList()) doAnswer { throw e }
+
+            val expected = connectionError
+            val actual = repository.getList()
+
+            assertEquals(expected, actual)
+        }
+
+    @Test
+    @Suppress("ktlint:standard:max-line-length")
+    fun `get with unknown exception EXPECT error type UNKNOWN`() = runTest(dispatcher) {
+        whenever(remoteDataSource.getList()) doAnswer { throw Exception() }
+
+        val expected = unknownError
+        val actual = repository.getList()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    @Suppress("ktlint:standard:max-line-length")
+    fun `get random EXPECT beer`() = runTest(dispatcher) {
+        whenever(remoteDataSource.getRandom()) doReturn beerDto
+        whenever(mapper.mapDtoToEntity(beerDto)) doReturn beer
+
+        val expected = Result.Success(beer)
+        val actual = repository.getRandom()
+
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @Suppress("ktlint:standard:max-line-length")
+    @MethodSource("connectionError")
+    fun `get random with Internet exception EXPECT connection error type`(e: Exception) =
+        runTest(dispatcher) {
+            whenever(remoteDataSource.getRandom()) doAnswer { throw e }
+
+            val expected = connectionError
+            val actual = repository.getRandom()
+
+            assertEquals(expected, actual)
+        }
+
+    @Test
+    @Suppress("ktlint:standard:max-line-length")
+    fun `get random with unknown exception EXPECT error type UNKNOWN`() = runTest(dispatcher) {
+        whenever(remoteDataSource.getRandom()) doAnswer { throw Exception() }
+
+        val expected = unknownError
+        val actual = repository.getRandom()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    @Suppress("ktlint:standard:max-line-length")
+    fun `get by id EXPECT beer`() = runTest(dispatcher) {
+        whenever(remoteDataSource.getById(id = 1L)) doReturn beerDto
+        whenever(mapper.mapDtoToEntity(beerDto)) doReturn beer
+
+        val expected = Result.Success(beer)
+        val actual = repository.getById(id = 1L)
+
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @Suppress("ktlint:standard:max-line-length")
+    @MethodSource("connectionError")
+    fun `get by id with Internet exception EXPECT connection error type`(e: Exception) =
+        runTest(dispatcher) {
+            whenever(remoteDataSource.getById(id = 1L)) doAnswer { throw e }
+
+            val expected = connectionError
+            val actual = repository.getById(id = 1L)
+
+            assertEquals(expected, actual)
+        }
+
+    @Test
+    @Suppress("ktlint:standard:max-line-length")
+    fun `get by id with unknown exception EXPECT error type UNKNOWN`() = runTest(dispatcher) {
+        whenever(remoteDataSource.getById(id = 1L)) doAnswer { throw Exception() }
+
+        val expected = unknownError
+        val actual = repository.getById(id = 1L)
 
         assertEquals(expected, actual)
     }
