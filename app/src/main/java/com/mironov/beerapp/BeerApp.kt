5165adfcb1c5a0da2dea5e1364d7d852cd8a72a8
Denis Mironov <denis5918@gmail.com>
2023-12-25 16:07:55 +0700

    configure koin

---
+++ app/src/main/java/com/mironov/beerapp/BeerApp.kt
@@ -0,0 +1,26 @@
+package com.mironov.beerapp
+
+import android.app.Application
+import com.mironov.beerapp.di.provideDataModule
+import com.mironov.beerapp.di.provideDatabaseModule
+import com.mironov.beerapp.di.provideDomainModule
+import com.mironov.beerapp.di.provideNetworkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class BeerApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@BeerApp)
+            modules(
+                provideDataModule(),
+                provideDomainModule(),
+                provideDatabaseModule(),
+                provideNetworkModule(),
+            )
+        }
+    }
+}
