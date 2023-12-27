b627dce8af710f1e7977087008efcab2e140c3a3
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-27 23:15:10 +0700

    Переписал тесты на RandomViewModel (turbine)

--- app/src/test/java/com/mironov/beerapp/presentation/RandomViewModelTest.kt
+++ app/src/test/java/com/mironov/beerapp/presentation/RandomViewModelTest.kt
@@ -7,12 +7,12 @@ import com.mironov.beerapp.presentation.random.RandomScreenState
 import com.mironov.beerapp.presentation.random.RandomViewModel
 import com.mironov.beerapp.util.BeerData
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.doReturnConsecutively
+import org.mockito.kotlin.doSuspendableAnswer
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import kotlin.test.assertEquals
@@ -52,68 +52,50 @@ class RandomViewModelTest {
         }
 
     @Test
-    fun `invoke view model getRandomBeer() twice and result return connection error and success EXPECT state changing from Initial to Error to Content`() =
+    fun `result return unknown error EXPECT state changing from Initial to Loading to Error`() =
         runTest {
-            whenever(getRandomBeerUseCase()) doReturnConsecutively listOf(
-                connectionError,
-                beerSuccess
-            )
+            whenever(getRandomBeerUseCase()) doSuspendableAnswer {
+                delay(1L)
+                unknownError
+            }
 
-            viewModel.state.test() {
-                viewModel.getRandomBeer()
+            viewModel.state.test {
                 viewModel.getRandomBeer()
                 assertEquals(RandomScreenState.Initial, awaitItem())
+                assertEquals(RandomScreenState.Loading, awaitItem())
+                assertEquals(RandomScreenState.Error(errorType = ErrorType.UNKNOWN), awaitItem())
+            }
+        }
+
+    @Test
+    fun `result return connection error EXPECT state changing from Initial to Loading to Error`() =
+        runTest {
+            whenever(getRandomBeerUseCase()) doSuspendableAnswer {
+                delay(1L)
+                connectionError
+            }
+
+            viewModel.state.test {
+                viewModel.getRandomBeer()
+                assertEquals(RandomScreenState.Initial, awaitItem())
+                assertEquals(RandomScreenState.Loading, awaitItem())
                 assertEquals(RandomScreenState.Error(errorType = ErrorType.CONNECTION), awaitItem())
+            }
+        }
+
+    @Test
+    fun `result return success EXPECT state changing from Initial to Loading to Content`() =
+        runTest {
+            whenever(getRandomBeerUseCase()) doSuspendableAnswer {
+                delay(1L)
+                beerSuccess
+            }
+
+            viewModel.state.test {
+                viewModel.getRandomBeer()
+                assertEquals(RandomScreenState.Initial, awaitItem())
+                assertEquals(RandomScreenState.Loading, awaitItem())
                 assertEquals(RandomScreenState.Content(content = beer), awaitItem())
             }
         }
-
-    @Test
-    fun `invoke view model getRandomBeer() twice and result return connection error twice EXPECT state changing from Initial to Error`() =
-        runTest {
-            whenever(getRandomBeerUseCase()) doReturnConsecutively listOf(
-                connectionError,
-                connectionError
-            )
-
-            viewModel.state.test() {
-                viewModel.getRandomBeer()
-                viewModel.getRandomBeer()
-                assertEquals(RandomScreenState.Initial, awaitItem())
-                assertEquals(RandomScreenState.Error(errorType = ErrorType.CONNECTION), awaitItem())
-            }
-        }
-
-    @Test
-    fun `result return unknown error EXPECT Error state`() = runTest {
-        whenever(getRandomBeerUseCase()) doReturn unknownError
-        viewModel.getRandomBeer()
-
-        val expected = RandomScreenState.Error(errorType = ErrorType.UNKNOWN)
-        val actual = viewModel.state.value
-
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `result return connection error EXPECT Error state`() = runTest {
-        whenever(getRandomBeerUseCase()) doReturn connectionError
-        viewModel.getRandomBeer()
-
-        val expected = RandomScreenState.Error(errorType = ErrorType.CONNECTION)
-        val actual = viewModel.state.value
-
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `result return success EXPECT Content state`() = runTest {
-        whenever(getRandomBeerUseCase()) doReturn beerSuccess
-        viewModel.getRandomBeer()
-
-        val expected = RandomScreenState.Content(content = beer)
-        val actual = viewModel.state.value
-
-        assertEquals(expected, actual)
-    }
 }
