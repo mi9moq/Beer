4b08384476155d1cfe44df8aac288476e6be408b
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-02-02 20:38:42 +0700

    Переписал screen

--- app/src/main/java/com/mironov/beerapp/navigation/Screen.kt
+++ app/src/main/java/com/mironov/beerapp/navigation/Screen.kt
@@ -1,22 +1,28 @@
 package com.mironov.beerapp.navigation
 
-import com.mironov.beerapp.domain.entity.Beer
-
 sealed class Screen(
     val route: String,
 ) {
     companion object {
+        const val KEY_BEER_ID = "beer_id"
+
+        private const val ROUTE_HOME = "home"
         private const val ROUTE_BEERS = "beers"
         private const val ROUTE_RANDOM_BEER = "random_beer"
-        private const val ROUTE_BEER_INFO = "beer_info"
+        private const val ROUTE_BEER_INFO = "beer_info/{$KEY_BEER_ID}"
     }
+
+    object Home : Screen(ROUTE_HOME)
 
     object Beers : Screen(ROUTE_BEERS)
 
     object Random : Screen(ROUTE_RANDOM_BEER)
 
     object Info : Screen(ROUTE_BEER_INFO) {
-        fun getRouteWithArgs(beer: Beer): String =
-            "$ROUTE_BEER_INFO/${beer.id}"
+
+        private const val ROUTE_FOR_ARGS = "beer_info"
+
+        fun getRouteWithArgs(beerId: String): String =
+            "$ROUTE_FOR_ARGS/$beerId"
     }
 }
